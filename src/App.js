355892c61c57990288bdb8d212c7d1c8410a4aba
NeoHuncho <william.guinaudie@gmail.com>
2020-12-02 17:33:33 +0100

    added textbox+ makeStyles

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
 import React from 'react'
+import './App.css';
+import {makeStyles} from '@material-ui/core/styles'
+
 import ButtonGroup from '@material-ui/core/ButtonGroup';
 import Button from '@material-ui/core/Button';
 import Checkbox from '@material-ui/core/Checkbox';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
+import TextField from '@material-ui/core/TextField'
 import SaveIcon from '@material-ui/icons/Save'
 import DeleteIcon from '@material-ui/icons/Delete'
+
+const useStyles = makeStyles ({
+  root:{
+    background: 'linear-gradient(45deg,#333,#999)',
+    border:0,
+    borderRadius: 15,
+    color: 'white',
+    padding: '5px 30px'
+  }
+})
+
+function ButtonStyled(){
+  const classes = useStyles();
+  return <Button className={classes.root}>Test Styled Button</Button>
+}
 
 function CheckboxExample(){
   const [checked,setChecked] =React.useState(true);
@@ -29,6 +48,13 @@ function CheckboxExample(){
 const App = () => {
   return (
     <>
+    <ButtonStyled />
+    <TextField
+    variant='filled'
+    color= 'secondary'
+    type='date'
+    label='Enter Date'
+    />
     <CheckboxExample />
       <ButtonGroup
         size='large'
