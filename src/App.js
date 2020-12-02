201ae00d8cad357ba0411d0d276781d3ee0d16a8
NeoHuncho <william.guinaudie@gmail.com>
2020-12-02 12:35:35 +0100

    added checkbox

--- src/App.js
+++ src/App.js
@@ -2,17 +2,27 @@ import React from 'react'
 import ButtonGroup from '@material-ui/core/ButtonGroup';
 import Button from '@material-ui/core/Button';
 import Checkbox from '@material-ui/core/Checkbox';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
 import SaveIcon from '@material-ui/icons/Save'
 import DeleteIcon from '@material-ui/icons/Delete'
 
 function CheckboxExample(){
   const [checked,setChecked] =React.useState(true);
   return(
-    <>
-    <Checkbox
+    <FormControlLabel 
+    control= {<Checkbox
     checked ={checked}
-    />
-    </>
+    icon ={<DeleteIcon />}
+    checkedIcon ={<SaveIcon />}
+    onChange= {(e) => setChecked(e.target.checked)}
+    color = 'primary'
+    inputProps={{
+      'aria-label':'secondary checkbox'
+    }}
+    />}
+    label= 'Testing Checkbox'
+   />
+
   )
 }
 
