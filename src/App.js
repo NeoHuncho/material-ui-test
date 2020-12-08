016bc483f4d63bbd4ba0bc1a7f5b5e6010104136
NeoHuncho <william.guinaudie@gmail.com>
2020-12-08 14:48:44 +0100

    added comments

--- src/App.js
+++ src/App.js
@@ -1,14 +1,47 @@
+//npm install @material-ui/core
 import React from 'react'
 import './App.css';
-import {makeStyles} from '@material-ui/core/styles'
+
+
+import {makeStyles, ThemeProvider, createMuiTheme} from '@material-ui/core/styles'
+import CssBaseline from "@material-ui/core/CssBaseline";
+
+import 'fontsource-roboto';//npm install fontsource-roboto 
+import Typography from '@material-ui/core/Typography'
+
 
 import ButtonGroup from '@material-ui/core/ButtonGroup';
 import Button from '@material-ui/core/Button';
 import Checkbox from '@material-ui/core/Checkbox';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
 import TextField from '@material-ui/core/TextField'
+import Container from '@material-ui/core/Container'
+
+import {orange,green} from '@material-ui/core/colors'
 import SaveIcon from '@material-ui/icons/Save'
 import DeleteIcon from '@material-ui/icons/Delete'
+
+const theme= createMuiTheme({//This will be imported below in the app compenent as the theme 
+  typography:{
+    h2:{
+      fontSize:36,
+    }
+  },
+  palette:{
+    background: {
+      default: "#222222"
+    },
+    text: {
+      primary: "#ffffff"
+    },
+    primary:{
+      main:orange[500]
+    },
+    secondary:{
+      main:green[500]
+    }
+  }
+})
 
 const useStyles = makeStyles ({
   root:{
@@ -47,7 +80,15 @@ function CheckboxExample(){
 
 const App = () => {
   return (
-    <>
+    <ThemeProvider theme={theme}>
+        <CssBaseline />
+      <Container maxWidth='md'>
+      <Typography variant='h2' component='div'>
+        Welcome to MUI
+      </Typography>
+      <Typography variant='subtitle1'>
+        Learning Material Ui
+      </Typography>
     <ButtonStyled />
     <TextField
     variant='filled'
@@ -76,7 +117,8 @@ const App = () => {
           Discard
           </Button>
       </ButtonGroup>
-    </>
+      </Container>
+    </ThemeProvider>
   )
 }
 
